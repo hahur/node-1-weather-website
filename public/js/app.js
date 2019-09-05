ca5cbadc68cbb7bf9de0c5b44663c789d9e52d92
hahur <suyash.saxena337@gmail.com>
2019-09-05 12:49:30 +0530

    Inint Commit

---
+++ public/js/app.js
@@ -0,0 +1,39 @@
+console.log('Client side javascript file is loaded!')
+
+const weatherForm = document.querySelector(".myForm")
+const search = document.querySelector(".myInput")
+const messageOne = document.querySelector(".messageOne")
+const messageTwo = document.querySelector(".messageTwo")
+const messageThree = document.querySelector(".messageThree")
+
+weatherForm.addEventListener("submit", (e) => {
+
+	e.preventDefault()
+
+	const location = search.value
+
+
+	if (location) {
+
+		const getWeatherData = () => {
+			return fetch(`http://localhost:3000/weather?address=${location}`)
+		}
+		getWeatherData().then(res => res.json()).then(json => {
+			if (json.error) {
+
+				messageThree.textContent = json.error
+
+			} else {
+				messageOne.textContent = json.location
+				messageTwo.textContent = json.forecast
+				messageThree.textContent = `The address you entered : "${json.address}"`
+
+			}
+		}).catch(err => console.log(err))
+
+
+	} else {
+		messageThree.textContent = "No data provided"
+	}
+
+})
---
+++ src/utils/geocode.js
@@ -0,0 +1,22 @@
+const request = require('request')
+
+const geocode = (address, callback) => {
+    const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + address + '.json?access_token=pk.eyJ1Ijoic3ByaXRlb3Bob2JpYWMiLCJhIjoiY2swNWpveWd1MDBnYjNwcWJxaWI4YzJ3dSJ9.HOF0VSxxRT5T0Xu15ad97g&limit=1'
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to location services!', undefined)
+        } else if (body.features.length === 0) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined, {
+                latitude: body.features[0].center[1],
+                longitude: body.features[0].center[0],
+                location: body.features[0].place_name
+            })
+        }
+    })
+}
+
+module.exports = geocode
+
